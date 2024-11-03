001afadfb6ed0ef3ab19272534133f87fd4132a7
kateqwerty001 <kateqwerty001@gmail.com>
2024-11-03 14:58:52 +0100

    some updage - not sure if 449 iterations is enough for such a big params grid...

--- RandomSearch/RandomSearchWithMetrics.py
+++ RandomSearch/RandomSearchWithMetrics.py
@@ -2,13 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import cross_val_predict, KFold
 from sklearn.metrics import f1_score, accuracy_score, roc_auc_score, brier_score_loss
-from scipy.stats import uniform
 import random
 
 class RandomSearchWithMetrics:
-    def __init__(self, pipeline, param_dist, X, y, n_iter=10, cv=5, path="", random_state=42):
+    def __init__(self, pipeline, params, X, y, n_iter=10, cv=5, path="", random_state=42, n_repeats=5):
         self.pipeline = pipeline
-        self.param_dist = param_dist
+        self.params = params
         self.X = X
         self.y = y
         self.n_iter = n_iter
@@ -16,16 +15,18 @@ class RandomSearchWithMetrics:
         self.history = []
         self.path = path
         self.random_state = random_state
+        self.n_repeats = n_repeats  # number of cross-validation rounds for stability, because using only one repeat gives very unstable results
 
     def generate_random_params(self):
         params = {}
-        for key, values in self.param_dist.items():
+        for key, values in self.params.items():
             if isinstance(values, list):
                 params[key] = random.choice(values)
         return params
 
     def fit_and_evaluate(self):
-        
+
+        # set seed in order to get the same parameters on all datasets
         random.seed(self.random_state)
         np.random.seed(self.random_state)
 
@@ -34,28 +35,31 @@ class RandomSearchWithMetrics:
             params = self.generate_random_params()
             self.pipeline.set_params(**params)
 
-            # cross-validation
-            kf = KFold(n_splits=self.cv, shuffle=True, random_state=self.random_state)
-            y_pred = cross_val_predict(self.pipeline, self.X, self.y, cv=kf, method='predict')
-            y_probabilities = cross_val_predict(self.pipeline, self.X, self.y, cv=kf, method='predict_proba')[:, 1]
+            # lists to accumulate metrics across rounds
+            f1_scores, accuracies, brier_scores, roc_aucs = [], [], [], []
 
-            # calculating metrics
-            f1 = f1_score(self.y, y_pred, average='weighted')
-            accuracy = accuracy_score(self.y, y_pred)
-            brier_score = brier_score_loss(self.y, y_probabilities)
-            roc_auc = roc_auc_score(self.y, y_probabilities)
+            for j in range(self.n_repeats):
+                kf = KFold(n_splits=self.cv, shuffle=True)
+                y_pred = cross_val_predict(self.pipeline, self.X, self.y, cv=kf, method='predict')
+                y_probabilities = cross_val_predict(self.pipeline, self.X, self.y, cv=kf, method='predict_proba')[:, 1]
 
-            # dictionary with metrics
-            metrics = {
-                'f1': f1,
-                'accuracy': accuracy,
-                'brier_score': brier_score,
-                'roc_auc': roc_auc
+                # calculate metrics
+                f1_scores.append(f1_score(self.y, y_pred, average='weighted'))
+                accuracies.append(accuracy_score(self.y, y_pred))
+                brier_scores.append(brier_score_loss(self.y, y_probabilities))
+                roc_aucs.append(roc_auc_score(self.y, y_probabilities))
+
+            # calculate average metrics across repeats
+            avg_metrics = {
+                'f1': np.mean(f1_scores),
+                'accuracy': np.mean(accuracies),
+                'brier_score': np.mean(brier_scores),
+                'roc_auc': np.mean(roc_aucs)
             }
 
-            # update history
-            metrics.update(params)
-            self.history.append(metrics)
+            # Update history with averaged metrics
+            avg_metrics.update(params)
+            self.history.append(avg_metrics)
 
             self.save_results(path_to_save=self.path)
 
